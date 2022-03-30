bc0ba16c1e4d2089c8504a3fcc3d173e7c2d4407
Juliana Peña <me@julip.co>
2022-03-30 14:28:35 -0700

    reponsive images

--- src/components/PhotoGridItem/PhotoGridItem.js
+++ src/components/PhotoGridItem/PhotoGridItem.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
 const PhotoGridItem = ({ id, src, alt, tags }) => {
   return (
     <article>
       <Anchor href={`/photos/${id}`}>
-        <Image src={src} />
+        <Picture>
+          <source
+            srcSet={`
+            ${src.replace(".jpg", ".avif")} 1x,
+            ${src.replace(".jpg", "@2x.avif")} 2x,
+            ${src.replace(".jpg", "@3x.avif")} 3x
+          `}
+            type="image/avif"
+          />
+          <source
+            srcSet={`
+            ${src} 1x,
+            ${src.replace(".jpg", "@2x.jpg")} 2x,
+            ${src.replace(".jpg", "@3x.jpg")} 3x
+          `}
+            type="image/jpg"
+          />
+          <Image src={src} alt="" />
+        </Picture>
       </Anchor>
       <Tags>
         {tags.map((tag) => (
@@ -22,12 +40,18 @@ const Anchor = styled.a`
   outline-offset: 4px;
 `;
 
-const Image = styled.img`
+const Picture = styled.picture`
   display: block;
   width: 100%;
   height: 300px;
   border-radius: 2px;
   margin-bottom: 8px;
+`;
+
+const Image = styled.img`
+  object-fit: cover;
+  width: 100%;
+  height: 100%;
 `;
 
 const Tags = styled.ul`
